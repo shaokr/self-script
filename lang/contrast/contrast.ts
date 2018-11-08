22cfac4a73c988dc3a87409416facccbe64848ce
shaokr <shaokangrun@91kook.com>
2018-11-08 11:03:32 +0800

    代码优化下~

--- lang/contrast/contrast.ts
+++ lang/contrast/contrast.ts
@@ -1,28 +1,29 @@
 const fp = require('lodash/fp');
 const fs = require('fs-extra');
 
-const enStr = fs.readFileSync(`${__dirname}/en.js`, 'utf-8');
-const zhStr = fs.readFileSync(`${__dirname}/zh.js`, 'utf-8');
-
 const reg = new RegExp(/([\S]+):[\s]+'(.+)'/, 'g');
-const listEn = [];
-const listZh = [];
-let match = [];
-while ((match = reg.exec(enStr)) !== null) {
-  listEn.push(match[1]);
+const getLangArr = (lang) => {
+  const _str = fs.readFileSync(`${__dirname}/${lang}.js`, 'utf-8');
+  const list = [];
+  let match = [];
+  while ((match = reg.exec(_str)) !== null) {
+    list.push(match[1]);
+  }
+  return list;
 }
-while ((match = reg.exec(zhStr)) !== null) {
-  listZh.push(match[1]);
-}
-const list = [['', 'en', 'zh']];
+const listEn = getLangArr('en');
+const listZh = getLangArr('zh');
 fp.flow([
   fp.concat,
   fp.max,
   fp.range(0),
   fp.map(index => {
-    if (listEn[index] !== listZh[index])
-      list.push([index, listEn[index], listZh[index]]);
-  })
+    if (listEn[index] !== listZh[index]) {
+      return [index, listEn[index], listZh[index]];
+    }
+  }),
+  fp.compact,
+  fp.concat([['', 'en', 'zh']]),
+  console.log
 ])(fp.size(listEn), fp.size(listZh));
-console.log(list);
 export {};
