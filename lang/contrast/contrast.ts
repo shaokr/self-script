e488f0bcfa522ee5ca01d2a47d66fe0608e194dd
shaokr <shaokangrun@91kook.com>
2018-11-08 11:04:32 +0800

    再优化下~

--- lang/contrast/contrast.ts
+++ lang/contrast/contrast.ts
@@ -2,7 +2,7 @@ const fp = require('lodash/fp');
 const fs = require('fs-extra');
 
 const reg = new RegExp(/([\S]+):[\s]+'(.+)'/, 'g');
-const getLangArr = (lang) => {
+const getLangArr = lang => {
   const _str = fs.readFileSync(`${__dirname}/${lang}.js`, 'utf-8');
   const list = [];
   let match = [];
@@ -10,18 +10,17 @@ const getLangArr = (lang) => {
     list.push(match[1]);
   }
   return list;
-}
+};
 const listEn = getLangArr('en');
 const listZh = getLangArr('zh');
 fp.flow([
   fp.concat,
   fp.max,
   fp.range(0),
-  fp.map(index => {
-    if (listEn[index] !== listZh[index]) {
-      return [index, listEn[index], listZh[index]];
-    }
-  }),
+  fp.map(
+    index =>
+      listEn[index] !== listZh[index] && [index, listEn[index], listZh[index]]
+  ),
   fp.compact,
   fp.concat([['', 'en', 'zh']]),
   console.log
